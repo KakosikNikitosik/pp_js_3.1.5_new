f0f4d70a65a7bc5df74f2df913d243fe79be50c8
KakosikNikitosik <thegoodness28@gmail.com>
2023-07-19 20:23:29 +0300

    Finished ver 1.3 rebuild runner, annotations are added

--- src/main/java/com/example/security/service/UserDetailsService.java
+++ src/main/java/com/example/security/service/UserDetailsService.java
@@ -9,11 +9,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 @Transactional(readOnly = true)
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
